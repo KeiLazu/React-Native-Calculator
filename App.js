bf583cc9e4357a7c94744734013497299d92a565
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-08-29 10:05:20 +0700

    changed calculator js into proper calculator, add headerapp previously calcualtor js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Calculator from './components/Calculator';
+import HeaderApp from './components/HeaderApp';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Calculator />
+        <HeaderApp />
       </View>
     );
   }
