2782a47508a84f67834e875eddd035ec6d1d11b0
Kennix Lazuardi <kennixlazuardi@gmail.com>
2018-08-29 13:21:52 +0700

    remove head

--- App.js
+++ App.js
@@ -4,24 +4,13 @@ import {
   Text
 } from 'react-native';
 
-<<<<<<< HEAD
-import Style from './styles/Style';
-import ReactCalculator from './components/ReactCalculator';
-=======
 import Calculator from './components/Calculator';
->>>>>>> parent of bf583cc... changed calculator js into proper calculator, add headerapp previously calcualtor js
 
 export default class App extends React.Component {
   render() {
     return (
-<<<<<<< HEAD
-      <View style={Style.rootContainer}>
-          <View style={Style.displayContainer}></View>
-          <View style={Style.inputContainer}></View>
-=======
       <View style={styles.container}>
         <Calculator />
->>>>>>> parent of bf583cc... changed calculator js into proper calculator, add headerapp previously calcualtor js
       </View>
     );
   }
